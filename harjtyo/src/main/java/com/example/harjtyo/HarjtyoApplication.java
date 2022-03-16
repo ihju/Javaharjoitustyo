6977ffb5ad005b6b1dae67dc08ee7ab1588a07d8
ihju <t0ihju00@students.oamk.fi>
2022-03-16 11:42:33 +0200

    Työ alustettu

---
+++ harjtyo/src/main/java/com/example/harjtyo/HarjtyoApplication.java
@@ -0,0 +1,13 @@
+package com.example.harjtyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarjtyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarjtyoApplication.class, args);
+	}
+
+}
---
+++ harjtyo/src/test/java/com/example/harjtyo/HarjtyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.harjtyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarjtyoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
